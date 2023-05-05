105ea5e429c64002b650f01943fcffc9b760275a
Vinicius <viniciusnp3610@gmail.com>
2023-05-05 19:02:04 -0300

    troca de backgrounds, inicio dos scripts de troca de nave

---
+++ Assets/Scripts/Generic/ShipMontage.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMontage : MonoBehaviour
+{
+    [SerializeField] List<GameObject> ships;
+    [SerializeField] List<Texture2D> textures;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
